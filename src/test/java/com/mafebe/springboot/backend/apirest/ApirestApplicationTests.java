6a67f0c2f9629b0751b793560250c87c6d5d9dba
Andres Benavides <dandavi2310@hotmail.com>
2024-01-24 12:51:09 -0500

    primer commit

---
+++ src/test/java/com/mafebe/springboot/backend/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafebe.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
